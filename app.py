5053dfc8064870132dc16518ecb4ee706f7220bf
AyoubAy24-meet <129674378+1904ayoub@users.noreply.github.com>
2023-07-31 14:36:25 +0300

    UPDATED

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import session as login_session
+import pyrebase
 
-app = Flask(__name__)
+config = {
+  "apiKey": "AIzaSyA_sYbPLjQre0ev6vRt1JORu3XKW17LWBY",
+  "authDomain": "tech2peace-e783d.firebaseapp.com",
+  "databaseURL": "https://tech2peace-e783d-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "tech2peace-e783d",
+  "storageBucket": "tech2peace-e783d.appspot.com",
+  "messagingSenderId": "392709059440",
+  "appId": "1:392709059440:web:928f231b1e63be219c55b5",
+  "measurementId": "G-BL7GJ7GH6Y"
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db=firebase.database()
 
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['SECRET_KEY'] = 'super-secret-key'
 @app.route('/')
 def index():
-    # Handle form submission here
-    first_name = request.form.get('first_name')
-    last_name = request.form.get('last_name')
-    email = request.form.get('email')
-    phone = request.form.get('phone')
     return render_template('index.html')
+
+@app.route('/apply', methods=['GET', 'POST'])
+def apply():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        age = request.form['age']
+        interest = request.form['interest']
+        message = request.form['message']
+        person = {"first_name": firstname,"last_name": lastname,"email": email,"phone": phone,"age": age,"interest": interest,"message": message}
+        db.child('People').push(person)
+    return render_template('apply.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
