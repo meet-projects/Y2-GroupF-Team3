8e1202f29965c2759cabfd9efadf75cbd2831019
AyoubAy24-meet <129674378+1904ayoub@users.noreply.github.com>
2023-08-01 16:52:47 +0300

    updated

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+
 config = {
   "apiKey": "AIzaSyA_sYbPLjQre0ev6vRt1JORu3XKW17LWBY",
   "authDomain": "tech2peace-e783d.firebaseapp.com",
@@ -33,9 +34,42 @@ def apply():
         age = request.form['age']
         interest = request.form['interest']
         message = request.form['message']
-        person = {"first_name": firstname,"last_name": lastname,"email": email,"phone": phone,"age": age,"interest": interest,"message": message}
+        card = request.form['card']
+        person = {"first_name": firstname,"last_name": lastname,"email": email,"phone": phone,"age": age,"interest": interest,"message": message,"card":card,"admin":False}
         db.child('People').push(person)
     return render_template('apply.html')
 
+@app.route('/donate', methods=['GET', 'POST'])
+def donate():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+        card = request.form['card']
+        amount=request.form['amount']
+        bank=request.form['bank']
+        donator = {"first_name": firstname,"last_name": lastname,"email": email,"phone": phone,"message": message,"card":card,"amount":amount,"bank":bank}
+        db.child('Donations').push(donator)
+    return render_template('donate.html')
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        card = request.form['card']       
+        if "ayoub"==firstname and "ayoub"==lastname and "1904ayoub@gmail.com"==email and "+9725491904574"==phone and "fpdmvpo1224"==card :
+            person1=db.child("People").get().val()
+            try:
+                donator1=db.child("Donations").get().val()
+                return render_template('info.html', person1=person1,donator1=donator1, is_donate = True)
+            except:
+                return render_template('info.html', person1=person1, is_donate = False)
+
+    return render_template("admin.html")
 if __name__ == '__main__':
     app.run(debug=True)
